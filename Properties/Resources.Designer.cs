68fba3162a61a0bae105c4792d78bf7cfb8e5582
SHanDCuiBin <SDCuiBin@outlook.com>
2021-07-05 15:32:39 +0800

    命名控件项目名称等调整

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MSComm.Properties {
+namespace SerialTransmission.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MSComm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MSComm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SerialTransmission.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MSComm.Properties {
+namespace SerialTransmission.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
